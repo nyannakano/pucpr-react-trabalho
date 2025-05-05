8381458e4296f9a3ea2fb4fb025aecacc6085da0
Gabriel Nakano <gabrielnakano123@gmail.com>
2025-05-05 16:37:38 -0300

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
-const firebaseConfig = {
+let firebaseConfig = {
     apiKey: "AIzaSyA-S0MN5KoJqasv3DjHqNH3bwhC6dEnxUM",
     authDomain: "pucpr-trabalho.firebaseapp.com",
     projectId: "pucpr-trabalho",
@@ -15,4 +15,4 @@ if(!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-export const auth = firebase.auth();
+export default firebase;
